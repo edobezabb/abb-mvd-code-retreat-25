2ae0ca10b51698c759f0334b7f4e894108a1ca92
edobezabb <149474261+edobezabb@users.noreply.github.com>
2025-01-10 10:05:02 +0000

    Added Python test coverage

--- starting_points/python/game_of_life_test.py
+++ starting_points/python/game_of_life_test.py
@@ -1,6 +1,6 @@
 import pytest  # noqa: F401
 
-from game_of_life import add
+from game_of_life import add, subtract
 
 
 def test_example():
@@ -9,3 +9,7 @@ def test_example():
 
 def test_add():
     assert add(1, 2) == 3
+
+
+def test_sub():
+    assert subtract(3, 1) == 2
